414ad24d9a6eb3313225c46a0358f84386014118
younghoonkim <younghoonkim@contractor.ea.com>
2025-05-10 19:14:26 +0900

    chore(0.0.2): key-factory 타입 추론 개선 & 스탭 감소 (2 -> 1)

--- src/key-factory.ts
+++ src/key-factory.ts
@@ -1,14 +1,19 @@
-import type { Tuple, KeyEntry } from "./types/common.type";
+import type { Tuple, KeyEntry, KeyFn } from "./types/common.type";
 
 /**
  * @description prefix를(Root) 붙여주는 래퍼 타입
  * @description 키 인자가 단순 배열이면 함수 형태로 반환 / 함수형태면 그대로
  */
-export type Prefixed<Root extends Tuple, E extends KeyEntry> = E extends (
-  ...a: infer A
-) => infer R
-  ? (...a: A) => readonly [...Root, ...(R & Tuple)]
-  : () => readonly [...Root, ...(E & Tuple)];
+type Prefixed<Root extends Tuple, E extends KeyEntry> = E extends KeyFn<
+  infer Tail,
+  infer A
+>
+  ? Tail extends Tuple
+    ? (...a: A) => readonly [...Root, ...Tail]
+    : never
+  : E extends Tuple
+  ? () => readonly [...Root, ...E]
+  : never;
 
 /**
  * @description prefix에 대한 사용자가 넘긴 객체를 키 객체 형태로 반환
@@ -22,33 +27,41 @@ todoKeys.DEFAULT(); // => ['todo']
 todoKeys.ITEM('42'); // => ['todo','item','42']
  * ```
  */
-export type Keys<Defs extends Record<string, KeyEntry>, Root extends Tuple> = {
+type Keys<Defs extends Record<string, KeyEntry>, Root extends Tuple> = {
   [K in keyof Defs]: Prefixed<Root, Defs[K]>;
 };
 
-export function createKeyFactory<const P extends string>(prefix: P) {
+export function createKeyFactory<const P extends string>(
+  prefix: P
+): <const D extends Record<string, KeyEntry>>(defs: D) => Keys<D, readonly [P]>;
+
+export function createKeyFactory<
+  const P extends string,
+  const D extends Record<string, KeyEntry>
+>(prefix: P, defs: D): Keys<D, readonly [P]>;
+
+export function createKeyFactory(
+  prefix: string,
+  defs?: Record<string, KeyEntry>
+) {
   const root = [prefix] as const;
-  type Root = typeof root;
 
-  function wrap<E extends KeyEntry>(entry: E): Prefixed<Root, E> {
-    return ((...args: any[]) => {
-      const tail =
-        typeof entry === "function"
-          ? (entry as (...a: any[]) => Tuple)(...args)
-          : (entry as Tuple);
+  const wrap =
+    (entry: KeyEntry) =>
+    (...args: any) => {
+      const tail = typeof entry === "function" ? entry(...args) : entry;
       return [...root, ...tail] as const;
-    }) as Prefixed<Root, E>;
-  }
+    };
 
-  return function defineKeys<const D extends Record<string, KeyEntry>>(
-    defs: D
-  ): Keys<D, Root> {
-    const out = {} as Keys<D, Root>;
-
-    (Object.keys(defs) as Array<keyof D>).forEach((k) => {
-      out[k] = wrap(defs[k]);
+  const define = <D extends Record<string, KeyEntry>>(
+    d: D
+  ): Keys<D, typeof root> => {
+    const out = {} as Keys<D, typeof root>;
+    (Object.keys(d) as Array<keyof D>).forEach((k) => {
+      out[k] = wrap(d[k]) as any;
     });
-
     return out;
   };
+
+  return defs ? define(defs) : define;
 }
--- src/types/common.type.ts
+++ src/types/common.type.ts
@@ -1,7 +1,10 @@
 export type Tuple = readonly unknown[];
+export type KeyFn<T extends Tuple = Tuple, A extends readonly any[] = any[]> = (
+  ...a: A
+) => T;
 /**
  * @description Key에 허용되는 타입: Tuple | Tuple을 반환하는 함수
  * - TODO: 이러면 최소한 빈배열은 있어야 키로 등록됨 -> 좀 더 직관적인 default 키 지정 방법 고려 필요,,
  * - 2025.05.08 joseph0926
  */
-export type KeyEntry = Tuple | ((...a: readonly any[]) => Tuple);
+export type KeyEntry<T extends Tuple = Tuple> = T | KeyFn<T>;
